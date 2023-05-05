a31a354bd798e3b0020360fbcc769bb41c017257
Maria <mashakh04@gmail.com>
2023-05-05 11:28:11 +0300

    visitsPage

--- src/api/VisitsService.ts
+++ src/api/VisitsService.ts
@@ -6,4 +6,26 @@ export class VisitsService {
   static async updateVisit(data: IVisit) {
     return await axios.put(`/visits/`, data, ReqConfig());
   }
+
+  static async createVisit(visit: IVisit) {
+    try {
+      const response = await axios.post<IVisit[]>(
+        "/visits/",
+        visit,
+        ReqConfig()
+      );
+
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+
+  static async deleteVisit(
+    id_student: number | string,
+    visit_date: Date | string
+  ) {
+    //console.log(data);
+    return await axios.delete(`/visits/${id_student}/${visit_date}`);
+  }
 }
--- src/pages/VisitsPage.tsx
+++ src/pages/VisitsPage.tsx
@@ -1,12 +1,15 @@
 import {
   Button,
   Card,
+  DatePicker,
   Form,
   Input,
   InputNumber,
   InputRef,
   Layout,
+  Popconfirm,
   Select,
+  Space,
   Table,
   message,
 } from "antd";
@@ -21,11 +24,30 @@ import { IVisit } from "../models/IVisit";
 import { StudentWithVisits } from "../models/IStudent";
 import { VisitsService } from "../api/VisitsService";
 
+import dayjs from "dayjs";
+import { Dayjs } from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+import { ColumnsType } from "antd/es/table";
+import { CloseOutlined } from "@ant-design/icons";
+import { useLocation } from "react-router-dom";
+dayjs.extend(customParseFormat);
+require("dayjs/locale/ru");
+
+const { RangePicker } = DatePicker;
+type RangeValue = [Dayjs | null, Dayjs | null] | null;
+
 const VisitsPage: FC = () => {
+  const location = useLocation();
+  const group_id = location.pathname.split("/")[2];
+
   const [cols1, setCols1] = useState([] as any[]);
   const [rows1, setRows1] = useState([] as any[]);
   const [cols2, setCols2] = useState([] as any[]);
   const [rows2, setRows2] = useState([] as any[]);
+
+  const [form1] = useForm();
+
+  const [form2] = useForm();
 
   const [messageApi, contextHolder] = message.useMessage();
 
@@ -43,14 +65,33 @@ const VisitsPage: FC = () => {
     });
   };
 
-  // const [editing, setEditing] = useState(false);
+  const [Interval1, setInterval1] = useState<RangeValue>([
+    dayjs().add(-1, "y"),
+    dayjs(),
+  ]);
 
-  // const form = useForm();
+  const [Interval2, setInterval2] = useState<RangeValue>([
+    dayjs().add(-1, "y"),
+    dayjs().add(1, "y"),
+  ]);
 
   const getdate = (v: IVisit) => ({
-    title: moment(v.visit_date).format("l"),
+    title: (
+      <>
+        {dayjs(v.visit_date).format("DD.MM.YYYY")}{" "}
+        <Popconfirm
+          title="Вы уверены?"
+          onConfirm={async () =>
+            await deleteVisitOrAttestation(v.visit_date, v.id_type)
+          }
+        >
+          <Button type="text" size="small" danger icon={<CloseOutlined />} />
+        </Popconfirm>
+      </>
+    ),
     dataIndex: v.visit_date.toLocaleString(),
     key: v.visit_date.toLocaleString(),
+    width: 150,
   });
 
   const getstudentVisits = (student: StudentWithVisits) => {
@@ -67,14 +108,15 @@ const VisitsPage: FC = () => {
   };
 
   const getVisitsData = async () => {
-    const { data } = await GroupsService.getGroupsVisits(
-      1,
-      "2021-10-06",
-      "2023-12-01"
-    );
-    //console.log(data);
+    if (Interval1 === null) return;
 
-    let cols1 = data[0].visits.map((v) => ({
+    const { data } = await GroupsService.getGroupsVisits(
+      group_id,
+      Interval1[0]?.format("YYYY-MM-DD") || "2020-01-01",
+      Interval1[1]?.format("YYYY-MM-DD") || dayjs().format("YYYY-MM-DD")
+    );
+
+    let cols1: ColumnsType = data[0].visits.map((v) => ({
       ...getdate(v),
       render: (text: any, record: any) => (
         <Select
@@ -92,6 +134,8 @@ const VisitsPage: FC = () => {
       title: "",
       dataIndex: "FIO",
       key: "FIO",
+      width: 200,
+      fixed: "left",
       render: (text) => <span>{text}</span>,
     });
 
@@ -100,6 +144,30 @@ const VisitsPage: FC = () => {
     let rows1 = data.map((student) => getstudentVisits(student));
 
     setRows1(rows1);
+  };
+
+  const deleteVisitOrAttestation = async (
+    visit_date: Date,
+    id_type: number
+  ) => {
+    console.log(visit_date);
+
+    const deleteVisit = async () => {
+      for (let student of rows1) {
+        console.log(student.key);
+        if (student.key) {
+          let isDeleted = await VisitsService.deleteVisit(
+            student.key,
+            visit_date
+          );
+        }
+      }
+    };
+
+    await deleteVisit().then(() => {
+      if (id_type === 1) getVisitsData();
+      else getAttestationData();
+    });
   };
 
   const updateVisits = async (
@@ -125,20 +193,27 @@ const VisitsPage: FC = () => {
   };
 
   const getAttestationData = async () => {
+    if (Interval2 === null) return;
     const { data } = await GroupsService.getGroupsAttestation(
-      1,
-      "2021-10-06",
-      "2023-12-01"
+      group_id,
+      Interval2[0]?.format("YYYY-MM-DD") || "2020-01-01",
+      Interval2[1]?.format("YYYY-MM-DD") || dayjs().format("YYYY-MM-DD")
     );
 
-    let cols2 = data[0].visits.map((v) => ({
+    let cols2: ColumnsType = data[0].visits.map((v) => ({
       ...getdate(v),
       render: (text: any, record: any) => (
         <InputNumber
           style={{ width: 80 }}
           defaultValue={text}
-          onBlur={(e) =>
-            updateVisits(record.key, 2, true, v.visit_date, +e.target.value)
+          onBlur={async (e) =>
+            await updateVisits(
+              record.key,
+              2,
+              true,
+              v.visit_date,
+              +e.target.value
+            )
           }
         />
       ),
@@ -148,52 +223,57 @@ const VisitsPage: FC = () => {
       title: "",
       dataIndex: "FIO",
       key: "FIO",
+      width: 150,
+      fixed: "left",
       render: (text) => <span>{text}</span>,
     });
     setCols2(cols2);
 
     let rows2 = data.map((student) => getstudentVisits(student));
     setRows2(rows2);
-    //console.log(rows);
+  };
+
+  const createVisitorAttestation = async (values: any) => {
+    const { visit_date, type } = values;
+    console.log(type);
+
+    const create = async () => {
+      try {
+        let isSuccess = true;
+        for (let student of rows1) {
+          if (student.key) {
+            let data: IVisit = {
+              id_student: student.key,
+              id_type: type,
+              visit_date: visit_date,
+              is_visited: type === 2,
+            };
+            if (type === 2) data.points = 0;
+            let isCreated = await VisitsService.createVisit(data);
+            isSuccess = isSuccess && isCreated;
+          }
+        }
+
+        if (isSuccess) success();
+        else error();
+      } catch {
+        error();
+      }
+    };
+
+    await create().then(() => {
+      if (type === 1) getVisitsData();
+      else getAttestationData();
+    });
   };
 
   useEffect(() => {
     getVisitsData();
-    getAttestationData();
-  }, []);
+  }, [Interval1]);
 
-  const columns = [
-    {
-      title: "",
-      dataIndex: "FIO",
-      key: "FIO",
-      editable: false,
-    },
-    {
-      title: "22.01.2023",
-      dataIndex: "20230122",
-      key: "20230122",
-      editable: true,
-    },
-    {
-      title: "2023-01-23",
-      dataIndex: "20230123",
-      key: "20230123",
-      editable: true,
-    },
-    {
-      title: "2023-01-24",
-      dataIndex: "20230124",
-      key: "20230124",
-      editable: true,
-    },
-    {
-      title: "2023-01-25",
-      dataIndex: "20230125",
-      key: "20230125",
-      editable: true,
-    },
-  ];
+  useEffect(() => {
+    getAttestationData();
+  }, [Interval2]);
 
   return (
     <>
@@ -213,7 +293,32 @@ const VisitsPage: FC = () => {
               title="Посещаемость"
               extra={<Button type="primary">Добавить ученика</Button>}
             >
+              <RangePicker
+                defaultValue={Interval1}
+                value={Interval1}
+                onChange={(value) => setInterval1(value)}
+                format={"DD.MM.YYYY"}
+                style={{ marginBottom: 10 }}
+              />
+
+              <Form
+                form={form1}
+                onFinish={createVisitorAttestation}
+                initialValues={{ type: 1, visit_date: dayjs() }}
+              >
+                <Space>
+                  <Form.Item name="visit_date">
+                    <DatePicker format={"DD.MM.YYYY"} />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button htmlType="submit">Добавить занятие</Button>
+                  </Form.Item>
+                  <Form.Item name="type" />
+                </Space>
+              </Form>
+
               <Table
+                scroll={{ x: 1500 }}
                 // loading={isLoading}
                 dataSource={rows1}
                 columns={cols1}
@@ -221,6 +326,29 @@ const VisitsPage: FC = () => {
             </Card>
 
             <Card title="Итоговая аттестация">
+              <RangePicker
+                defaultValue={Interval2}
+                value={Interval2}
+                onChange={(value) => setInterval2(value)}
+                format={"DD.MM.YYYY"}
+                style={{ marginBottom: 10 }}
+              />
+
+              <Form
+                form={form2}
+                onFinish={createVisitorAttestation}
+                initialValues={{ type: 2, visit_date: dayjs() }}
+              >
+                <Space>
+                  <Form.Item name="visit_date">
+                    <DatePicker format={"DD.MM.YYYY"} />
+                  </Form.Item>
+                  <Form.Item>
+                    <Button htmlType="submit">Добавить результат</Button>
+                  </Form.Item>
+                  <Form.Item name="type" />
+                </Space>
+              </Form>
               <Table
                 // loading={isLoading}
                 dataSource={rows2}
